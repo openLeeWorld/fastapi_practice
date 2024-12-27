069ad221dc5e98f9b2c15062a6f3c65ea07ccf24
openLeeWorld <167273831+openLeeWorld@users.noreply.github.com>
2024-12-28 00:46:01 +0900

    Update auth_practice.py

--- ch8/src/auth_practice.py
+++ ch8/src/auth_practice.py
@@ -4,15 +4,16 @@ from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 app = FastAPI()
 
+maybe: str = "whodis?"
 user: str = "newphone"
-PmaybeW: str = "whodis?"
+
 
 basic: HTTPBasicCredentials = HTTPBasic() # 아이디, 비번의 기본 인증
 
 @app.get("/who")
 def get_user(creds: HTTPBasicCredentials = Depends(basic)) -> dict:
     if (creds.username == user and 
-        creds.password == PmaybeW):
+        creds.password == maybe):
         return {"username": creds.username, "password": creds.password}
     
     raise HTTPException(status_code=401, detail="Hey!")
