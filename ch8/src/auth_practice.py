ca5ba973f48060ce7455e7d8ebfd479dd5ecac66
openLeeWorld <167273831+openLeeWorld@users.noreply.github.com>
2024-12-28 00:42:18 +0900

    Update and rename auth._practice.py to auth_practice.py

--- ch8/src/auth_practice.py
+++ ch8/src/auth_practice.py
@@ -4,15 +4,15 @@ from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 app = FastAPI()
 
-secret_user: str = "newphone"
-secret_password: str = "whodis?"
+user: str = "newphone"
+maybepw: str = "whodis?"
 
 basic: HTTPBasicCredentials = HTTPBasic() # 아이디, 비번의 기본 인증
 
 @app.get("/who")
 def get_user(creds: HTTPBasicCredentials = Depends(basic)) -> dict:
-    if (creds.username == secret_user and 
-        creds.password == secret_password):
+    if (creds.username == user and 
+        creds.password == maybepw):
         return {"username": creds.username, "password": creds.password}
     
     raise HTTPException(status_code=401, detail="Hey!")
